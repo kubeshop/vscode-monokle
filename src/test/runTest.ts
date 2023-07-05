161581ab1050dd6bf4658956d539a8fdc1cf7539
f1ames <dr.odpowiedz@gmail.com>
2023-07-05 15:01:35 +0200

    chore: fix formatting in tests

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -4,87 +4,87 @@ import * as fs from 'fs/promises';
 import { downloadAndUnzipVSCode, resolveCliArgsFromVSCodeExecutablePath, runTests } from '@vscode/test-electron';
 
 async function main() {
-	try {
-		// The folder containing the Extension Manifest package.json
-		// Passed to `--extensionDevelopmentPath`
-		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+  try {
+    // The folder containing the Extension Manifest package.json
+    // Passed to `--extensionDevelopmentPath`
+    const extensionDevelopmentPath = path.resolve(__dirname, '../../');
 
-		// The path to test runner
-		// Passed to --extensionTestsPath
-		const extensionTestsPath = path.resolve(__dirname, './suite/index');
+    // The path to test runner
+    // Passed to --extensionTestsPath
+    const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
-		// Test fixtures
-		const testTmpDir = path.resolve(__dirname, './tmp');
-		const fixturesDestDir = path.resolve(testTmpDir, './fixtures');
-		const fixturesSourceDir = path.resolve(extensionDevelopmentPath, './src/test/fixtures');
+    // Test fixtures
+    const testTmpDir = path.resolve(__dirname, './tmp');
+    const fixturesDestDir = path.resolve(testTmpDir, './fixtures');
+    const fixturesSourceDir = path.resolve(extensionDevelopmentPath, './src/test/fixtures');
 
-		const vscodeExecutablePath = await downloadAndUnzipVSCode('stable');
-		const [cliPath, ...args] = resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
+    const vscodeExecutablePath = await downloadAndUnzipVSCode('stable');
+    const [cliPath, ...args] = resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
 
-		// Use cp.spawn / cp.exec for custom setup
-		cp.spawnSync(
-			cliPath,
-			[...args, '--install-extension', 'ms-sarifvscode.sarif-viewer@3.3.8'],
-			{
-				encoding: 'utf-8',
-				stdio: 'inherit'
-			}
-		);
+    // Use cp.spawn / cp.exec for custom setup
+    cp.spawnSync(
+      cliPath,
+      [...args, '--install-extension', 'ms-sarifvscode.sarif-viewer@3.3.8'],
+      {
+        encoding: 'utf-8',
+        stdio: 'inherit'
+      }
+    );
 
-		const workspaces = [
-			{
-				path: './folder-with-single-resource',
-				resources: 1,
-				config: 'default',
-			},
-			{
-				path: './folder-without-resources',
-				resources: 0,
-				config: 'file',
-			},
-			{
-				path: './workspace-1/workspace-1.code-workspace',
-				resources: 2,
-				isWorkspace: true,
-				folders: 2,
-				config: 'config'
-			},
-		];
+    const workspaces = [
+      {
+        path: './folder-with-single-resource',
+        resources: 1,
+        config: 'default',
+      },
+      {
+        path: './folder-without-resources',
+        resources: 0,
+        config: 'file',
+      },
+      {
+        path: './workspace-1/workspace-1.code-workspace',
+        resources: 2,
+        isWorkspace: true,
+        folders: 2,
+        config: 'config'
+      },
+    ];
 
-		for (const workspace of workspaces) {
-			await fs.rm(testTmpDir, { recursive: true, force: true });
-			await fs.mkdir(testTmpDir, { recursive: true });
-			await fs.cp(fixturesSourceDir, fixturesDestDir, { recursive: true });
+    for (const workspace of workspaces) {
+      await fs.rm(testTmpDir, { recursive: true, force: true });
+      await fs.mkdir(testTmpDir, { recursive: true });
+      await fs.cp(fixturesSourceDir, fixturesDestDir, { recursive: true });
 
-			const testWorkspace = path.resolve(fixturesDestDir, workspace.path);
+      const testWorkspace = path.resolve(fixturesDestDir, workspace.path);
 
-			console.log('Running tests for workspace:', testWorkspace);
+      console.log('Running tests for workspace:', testWorkspace);
 
-			await runTests({
-				vscodeExecutablePath,
-				extensionDevelopmentPath,
-				extensionTestsPath,
-				launchArgs: [testWorkspace],
-				extensionTestsEnv: {
-					// eslint-disable-next-line @typescript-eslint/naming-convention
-					EXTENSION_DIR: extensionDevelopmentPath,
-					// eslint-disable-next-line @typescript-eslint/naming-convention
-					FIXTURES_SOURCE_DIR: fixturesSourceDir,
-					// eslint-disable-next-line @typescript-eslint/naming-convention
-					WORKSPACE_RESOURCES: workspace.resources.toString(),
-					// eslint-disable-next-line @typescript-eslint/naming-convention
-					ROOT_PATH: testWorkspace,
-					// eslint-disable-next-line @typescript-eslint/naming-convention
-					WORKSPACE_CONFIG_TYPE: workspace.config,
-				}
-			});
-		}
-	} catch (err) {
-		console.error('Failed to run tests', err);
-		process.exit(1);
-	} finally {
-		await fs.rm(path.resolve(__dirname, './tmp'), { recursive: true, force: true });
-	}
+      await runTests({
+        vscodeExecutablePath,
+        extensionDevelopmentPath,
+        extensionTestsPath,
+        launchArgs: [testWorkspace],
+        extensionTestsEnv: {
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          EXTENSION_DIR: extensionDevelopmentPath,
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          FIXTURES_SOURCE_DIR: fixturesSourceDir,
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          WORKSPACE_RESOURCES: workspace.resources.toString(),
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          ROOT_PATH: testWorkspace,
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          WORKSPACE_CONFIG_TYPE: workspace.config,
+        }
+      });
+    }
+  } catch (err) {
+    console.error('Failed to run tests', err);
+    process.exit(1);
+  } finally {
+    await fs.rm(path.resolve(__dirname, './tmp'), { recursive: true, force: true });
+  }
 }
 
 main();
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -8,188 +8,188 @@ import { getValidationResult } from '../../utils/validation';
 import { generateId } from '../../utils/helpers';
 
 suite(`Monokle Extension Test Suite: ${process.env.ROOT_PATH}`, () => {
-	const extensionDir = process.env.EXTENSION_DIR;
-	const sharedStorageDir = path.resolve(extensionDir, '.monokle');
-	const fixturesSourceDir = process.env.FIXTURES_SOURCE_DIR;
+  const extensionDir = process.env.EXTENSION_DIR;
+  const sharedStorageDir = path.resolve(extensionDir, '.monokle');
+  const fixturesSourceDir = process.env.FIXTURES_SOURCE_DIR;
 
-	const initialResources = parseInt(process.env.WORKSPACE_RESOURCES ?? '0', 10);
+  const initialResources = parseInt(process.env.WORKSPACE_RESOURCES ?? '0', 10);
 
-	setup(async () => {
-		await fs.rm(sharedStorageDir, { recursive: true, force: true });
-	});
+  setup(async () => {
+    await fs.rm(sharedStorageDir, { recursive: true, force: true });
+  });
 
-	suiteTeardown(async () => {
-		await fs.rm(sharedStorageDir, { recursive: true, force: true });
-	});
+  suiteTeardown(async () => {
+    await fs.rm(sharedStorageDir, { recursive: true, force: true });
+  });
 
-	// This test should be run first since it checks for a result file
-	// created on VSCode instance start.
-	test('Validates resources on start', async function () {
-		if (initialResources === 0) {
-			this.skip();
-		}
+  // This test should be run first since it checks for a result file
+  // created on VSCode instance start.
+  test('Validates resources on start', async function () {
+    if (initialResources === 0) {
+      this.skip();
+    }
 
-		const folders = getWorkspaceFolders();
+    const folders = getWorkspaceFolders();
 
-		await assertEmptyValidationResults(extensionDir, folders[0]);
+    await assertEmptyValidationResults(extensionDir, folders[0]);
 
-		const result = await waitForValidationResults(extensionDir, folders[0]);
-		assertValidationResults(result);
-	}).timeout(1000 * 15);
+    const result = await waitForValidationResults(extensionDir, folders[0]);
+    assertValidationResults(result);
+  }).timeout(1000 * 15);
 
-	test('Does not run validation on start when no resources', async function () {
-		if (initialResources > 0) {
-			this.skip();
-		}
+  test('Does not run validation on start when no resources', async function () {
+    if (initialResources > 0) {
+      this.skip();
+    }
 
-		const folders = getWorkspaceFolders();
+    const folders = getWorkspaceFolders();
 
-		await assertEmptyValidationResults(extensionDir, folders[0]);
+    await assertEmptyValidationResults(extensionDir, folders[0]);
 
-		const result = await waitForValidationResults(extensionDir, folders[0], 1000 * 10);
-		assert.strictEqual(result, null);
-	}).timeout(1000 * 15);
+    const result = await waitForValidationResults(extensionDir, folders[0], 1000 * 10);
+    assert.strictEqual(result, null);
+  }).timeout(1000 * 15);
 
-	test('Exposes validate command', async function() {
-		const commands = await vscode.commands.getCommands(false);
-		assert.ok(commands.includes('monokle-vsc.validate'));
-	});
+  test('Exposes validate command', async function() {
+    const commands = await vscode.commands.getCommands(false);
+    assert.ok(commands.includes('monokle-vsc.validate'));
+  });
 
-	test('Exposes watch command', async () => {
-		const commands = await vscode.commands.getCommands(false);
-		assert.ok(commands.includes('monokle-vsc.watch'));
-	});
+  test('Exposes watch command', async () => {
+    const commands = await vscode.commands.getCommands(false);
+    assert.ok(commands.includes('monokle-vsc.watch'));
+  });
 
-	test('Validates resources on command run', async function() {
-		if (initialResources === 0) {
-			this.skip();
-		}
+  test('Validates resources on command run', async function() {
+    if (initialResources === 0) {
+      this.skip();
+    }
 
-		const folders = getWorkspaceFolders();
+    const folders = getWorkspaceFolders();
 
-		await assertEmptyValidationResults(extensionDir, folders[0]);
+    await assertEmptyValidationResults(extensionDir, folders[0]);
 
-		await vscode.commands.executeCommand('monokle-vsc.validate');
+    await vscode.commands.executeCommand('monokle-vsc.validate');
 
-		const result = await waitForValidationResults(extensionDir, folders[0]);
-		assertValidationResults(result);
-	}).timeout(1000 * 10);
+    const result = await waitForValidationResults(extensionDir, folders[0]);
+    assertValidationResults(result);
+  }).timeout(1000 * 10);
 
-	test('Does not run validation on command when no resources', async function() {
-		if (initialResources > 0) {
-			this.skip();
-		}
+  test('Does not run validation on command when no resources', async function() {
+    if (initialResources > 0) {
+      this.skip();
+    }
 
-		const folders = getWorkspaceFolders();
+    const folders = getWorkspaceFolders();
 
-		await assertEmptyValidationResults(extensionDir, folders[0]);
+    await assertEmptyValidationResults(extensionDir, folders[0]);
 
-		await vscode.commands.executeCommand('monokle-vsc.validate');
+    await vscode.commands.executeCommand('monokle-vsc.validate');
 
-		const result = await waitForValidationResults(extensionDir, folders[0], 1000 * 7);
-		assert.strictEqual(result, null);
-	}).timeout(1000 * 10);
+    const result = await waitForValidationResults(extensionDir, folders[0], 1000 * 7);
+    assert.strictEqual(result, null);
+  }).timeout(1000 * 10);
 
-	test('Validates resources on change (modification)', async function() {
-		if (initialResources === 0) {
-			this.skip();
-		}
+  test('Validates resources on change (modification)', async function() {
+    if (initialResources === 0) {
+      this.skip();
+    }
 
-		const folders = getWorkspaceFolders();
+    const folders = getWorkspaceFolders();
 
-		await assertEmptyValidationResults(extensionDir, folders[0]);
+    await assertEmptyValidationResults(extensionDir, folders[0]);
 
-		const file = path.resolve(folders[0].uri.fsPath, 'pod-errors.yaml');
-		const content = await fs.readFile(file, 'utf-8');
-		const asYaml = parse(content);
+    const file = path.resolve(folders[0].uri.fsPath, 'pod-errors.yaml');
+    const content = await fs.readFile(file, 'utf-8');
+    const asYaml = parse(content);
 
-		asYaml.apiVersion = 'v1beta1';
+    asYaml.apiVersion = 'v1beta1';
 
-		await fs.writeFile(file, stringify(asYaml));
+    await fs.writeFile(file, stringify(asYaml));
 
-		const result = await waitForValidationResults(extensionDir, folders[0]);
-		assertValidationResults(result);
-	}).timeout(1000 * 10);
+    const result = await waitForValidationResults(extensionDir, folders[0]);
+    assertValidationResults(result);
+  }).timeout(1000 * 10);
 
-	test('Validates resources on change (addition)', async () => {
-		const folders = getWorkspaceFolders();
+  test('Validates resources on change (addition)', async () => {
+    const folders = getWorkspaceFolders();
 
-		await assertEmptyValidationResults(extensionDir, folders[0]);
+    await assertEmptyValidationResults(extensionDir, folders[0]);
 
-		const newResource = path.resolve(fixturesSourceDir, 'sample-resource.yaml');
+    const newResource = path.resolve(fixturesSourceDir, 'sample-resource.yaml');
 
-		await fs.copyFile(newResource, path.resolve(folders[0].uri.fsPath, 'new-resource.yaml'));
+    await fs.copyFile(newResource, path.resolve(folders[0].uri.fsPath, 'new-resource.yaml'));
 
-		// how to make sure that new resource was validated?
-		const result = await waitForValidationResults(extensionDir, folders[0]);
-		assertValidationResults(result);
-	});
+    // how to make sure that new resource was validated?
+    const result = await waitForValidationResults(extensionDir, folders[0]);
+    assertValidationResults(result);
+  });
 
-	test('Validates resources on change (deletion)', async function () {
-		if (initialResources === 0) {
-			this.skip();
-		}
+  test('Validates resources on change (deletion)', async function () {
+    if (initialResources === 0) {
+      this.skip();
+    }
 
-		const folders = getWorkspaceFolders();
+    const folders = getWorkspaceFolders();
 
-		await assertEmptyValidationResults(extensionDir, folders[0]);
+    await assertEmptyValidationResults(extensionDir, folders[0]);
 
-		const existingResource = path.resolve(folders[0].uri.fsPath, 'cronjob.yml');
+    const existingResource = path.resolve(folders[0].uri.fsPath, 'cronjob.yml');
 
-		await fs.rm(existingResource);
+    await fs.rm(existingResource);
 
-		// how to make sure that new resource was validated?
-		const result = await waitForValidationResults(extensionDir, folders[0]);
-		assertValidationResults(result);
-	});
+    // how to make sure that new resource was validated?
+    const result = await waitForValidationResults(extensionDir, folders[0]);
+    assertValidationResults(result);
+  });
 
-	test('Uses correct validation config', async function () {
-		if (!process.env.WORKSPACE_CONFIG_TYPE) {
-			this.skip();
-		}
+  test('Uses correct validation config', async function () {
+    if (!process.env.WORKSPACE_CONFIG_TYPE) {
+      this.skip();
+    }
 
-		const folders = getWorkspaceFolders();
-		const config = await getWorkspaceConfig(folders[0]);
+    const folders = getWorkspaceFolders();
+    const config = await getWorkspaceConfig(folders[0]);
 
-		assert.equal(config.type, process.env.WORKSPACE_CONFIG_TYPE);
-	});
+    assert.equal(config.type, process.env.WORKSPACE_CONFIG_TYPE);
+  });
 });
 
 async function assertEmptyValidationResults(extensionDir, workspaceFolder: Folder): Promise<void> {
-	const result = await getValidationResult(extensionDir, generateId(workspaceFolder.uri.fsPath));
+  const result = await getValidationResult(extensionDir, generateId(workspaceFolder.uri.fsPath));
 
-	assert.ok(result === null);
+  assert.ok(result === null);
 }
 
 async function waitForValidationResults(extensionDir, workspaceFolder: Folder, timeoutMs?: number): Promise<any> {
-	return new Promise((res) => {
-		let timeoutId = null;
+  return new Promise((res) => {
+    let timeoutId = null;
 
-		const intervalId = setInterval(async () => {
-			const result = await getValidationResult(extensionDir, generateId(workspaceFolder.uri.fsPath));
+    const intervalId = setInterval(async () => {
+      const result = await getValidationResult(extensionDir, generateId(workspaceFolder.uri.fsPath));
 
-			if (result) {
-				clearInterval(intervalId);
-				timeoutId && clearTimeout(timeoutId);
-				res(result);
-			}
-		}, 250);
+      if (result) {
+        clearInterval(intervalId);
+        timeoutId && clearTimeout(timeoutId);
+        res(result);
+      }
+    }, 250);
 
-		if (timeoutMs) {
-			timeoutId = setTimeout(() => {
-				clearInterval(intervalId);
-				res(null);
-			}, timeoutMs);
-		}
-	});
+    if (timeoutMs) {
+      timeoutId = setTimeout(() => {
+        clearInterval(intervalId);
+        res(null);
+      }, timeoutMs);
+    }
+  });
 }
 
 function assertValidationResults(result) {
-	assert.ok(result);
-	assert.ok(result?.runs);
-	assert.ok(result.runs[0].tool);
-	assert.ok(result.runs[0].results);
-	assert.ok(result.runs[0].taxonomies);
+  assert.ok(result);
+  assert.ok(result?.runs);
+  assert.ok(result.runs[0].tool);
+  assert.ok(result.runs[0].results);
+  assert.ok(result.runs[0].taxonomies);
 }
 
 // @TODO cover below scenarios:
