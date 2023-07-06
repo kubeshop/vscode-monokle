ac97601f3d55149ee7e5980f1a9d0f50e68217d8
f1ames <dr.odpowiedz@gmail.com>
2023-07-06 16:59:00 +0200

    feat: add folder selector on 'show config' command

--- src/commands/show-configuration.ts
+++ src/commands/show-configuration.ts
@@ -1,8 +1,14 @@
-import { Uri, commands } from 'vscode';
+import { Uri, commands, window } from 'vscode';
 import { canRun } from '../utils/commands';
 import { getWorkspaceConfig, getWorkspaceFolders } from '../utils/workspace';
 import { createTemporaryConfigFile } from '../utils/validation';
 import type { RuntimeContext } from '../utils/runtime-context';
+import type { Folder } from '../utils/workspace';
+
+type FolderItem = {
+  label: string;
+  id: string;
+};
 
 export function getShowConfigurationCommand(context: RuntimeContext) {
   return async () => {
@@ -18,12 +24,38 @@ export function getShowConfigurationCommand(context: RuntimeContext) {
 
     const configs = await Promise.all(folders.map(async (folder) => await getWorkspaceConfig(folder)));
 
-    for (const config of configs) {
+    const showConfig = async (folder: Folder) => {
+      const config = configs.find(config => config.owner.id === folder.id);
       const configUri = config.type === 'file' || config.type === 'config' ?
         Uri.file(config.path) :
         await createTemporaryConfigFile(config.config, config.owner, context.extensionContext.extensionPath);
 
-      await commands.executeCommand('vscode.open', configUri);
+      return commands.executeCommand('vscode.open', configUri);
+    };
+
+    if (folders.length === 1) {
+      return await showConfig(folders[0]);
     }
+
+    const quickPick = window.createQuickPick<FolderItem>();
+
+    quickPick.items = folders.map(folder => ({ label: folder.name, id: folder.id }));
+    quickPick.onDidChangeSelection(async (selection) => {
+      if (selection.length > 0) {
+        const selectedFolder = folders.find(folder => folder.id === selection[0].id);
+
+        if (!selectedFolder) {
+          // error
+        }
+
+        quickPick.hide();
+
+        await showConfig(selectedFolder);
+      }
+    });
+    quickPick.onDidHide(() => quickPick.dispose());
+    quickPick.show();
+
+    return null;
   };
 }
