2fbf0254849f154ba609edf5403d394f203d81a9
f1ames <dr.odpowiedz@gmail.com>
2023-12-12 18:12:25 +0100

    fix: use resource guess when extracting

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -1,4 +1,4 @@
 export async function extractK8sResources(files: any[]) {
   const { extractK8sResources: extract } = await import('@monokle/parser');
-  return extract(files, false);
+  return extract(files, false, true);
 }
